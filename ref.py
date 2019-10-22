0b9ab7fef2ac66c01b6bc3c71620d074b02e4bfb
jerry800416 <s123962474@gmail.com>
2019-10-22 18:14:04 +0800

    新增ref :去掉敏感資訊

---
+++ ref.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# db_connect
+db_info = ()
+db_info2 = ()
+insert_db_name = "DTR_realtime_cwb"
+sql = "SELECT DISTINCT SegID FROM `Relation` WHERE node_status = 1"
+log_path = './cwb.log'
+# data_path = './cwb/'
+data_path = '/home/cwb/' 
